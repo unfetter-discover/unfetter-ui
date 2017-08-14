61aa0dac6d2704eef880e6a0419274e15661776f
j987987 <jason@purplelabs.io>
2017-08-14 15:22:34 -0400

    general CSS refactoring and fixes

--- src/vendor.browser.ts
+++ src/vendor.browser.ts
@@ -12,10 +12,10 @@ import 'rxjs';
 // Other vendors for example jQuery, Lodash or Bootstrap
 // You can import js, ts, css, sass, ...
 import 'hammerjs';
-import 'jquery';
+// import 'jquery';
 import 'bootstrap/dist/js/bootstrap';
-import 'bootstrap-toggle/css/bootstrap-toggle.min.css';
-import 'bootstrap-toggle/js/bootstrap-toggle.min.js';
+// import 'bootstrap-toggle/css/bootstrap-toggle.min.css';
+// import 'bootstrap-toggle/js/bootstrap-toggle.min.js';
 import 'primeng/primeng';
 import 'd3';
 import 'perfect-scrollbar';
