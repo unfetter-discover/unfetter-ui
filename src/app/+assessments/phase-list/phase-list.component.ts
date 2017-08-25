5b9cbcdd197aa3b0db8150249d4e2c68f0687adf
j987987 <jason@purplelabs.io>
2017-08-25 16:02:56 -0400

    rewrote assessments dashboard to new risk by attack pattern api format, fixed various calculation errors

--- src/app/+assessments/phase-list/phase-list.component.ts
+++ src/app/+assessments/phase-list/phase-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Constance } from '../../utils/constance';
 
@@ -8,13 +8,21 @@ import { Constance } from '../../utils/constance';
     styleUrls: ['./phase-list.component.css']
 })
 
-export class PhaseListComponent {
+export class PhaseListComponent implements OnInit {
 
     @Input('phase') public phase: any;
     @Input('numAttackPatterns') public numAttackPatterns: any;
-    @Input('assessmentId') public assessmentId: any;
-    @Input('questions') public questions: any;
     @Input('riskBreakdown') public riskBreakdown: any;
 
+    private totalRisk: number = 1;
+
     constructor(private route: ActivatedRoute) { }
+
+    ngOnInit() {
+        let tempRisk = 0;
+        for (let question in this.riskBreakdown) {
+            tempRisk += this.riskBreakdown[question];
+        }
+        this.totalRisk = tempRisk;
+    }
 }
