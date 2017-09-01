dbe18fcafa74daad99bbb3d74e8a03d9d6fe7039
mushinlogit <mushinlogit@gmail.com>
2017-09-01 09:38:57 -0400

    fix missing snackbar for reports

--- src/app/settings/stix-objects/reports/report-new/report-new.component.ts
+++ src/app/settings/stix-objects/reports/report-new/report-new.component.ts
@@ -2,30 +2,31 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
-import { MdDialog, MdDialogRef, MdDialogConfig } from '@angular/material';
+import { MdDialog, MdDialogRef, MdDialogConfig, MdSnackBar } from '@angular/material';
 import { BaseStixComponent } from '../../../base-stix.component';
 import { StixService } from '../../../stix.service';
 import { Report, AttackPattern, IntrusionSet, Relationship } from '../../../../models';
 
 @Component({
-  selector: 'report-new',
-  templateUrl: './report-new.component.html',
+    selector: 'report-new',
+    templateUrl: './report-new.component.html',
 })
 export class ReportNewComponent extends BaseStixComponent implements OnInit {
 
-  public report: Report = new Report();
-  public attackPatterns: AttackPattern[] = [];
-  public intrusionSets: IntrusionSet[] = [];
-  public labels = ['Indicator', 'Campaign', 'Intrusion Set' ];
+    public report: Report = new Report();
+    public attackPatterns: AttackPattern[] = [];
+    public intrusionSets: IntrusionSet[] = [];
+    public labels = ['Indicator', 'Campaign', 'Intrusion Set'];
 
-   constructor(
+    constructor(
         public stixService: StixService,
         public route: ActivatedRoute,
         public router: Router,
         public dialog: MdDialog,
-        public location: Location) {
+        public location: Location,
+        public snackBar: MdSnackBar) {
 
-        super(stixService, route, router, dialog, location);
+        super(stixService, route, router, dialog, location, snackBar);
         stixService.url = this.report.url;
     }
     public ngOnInit() {
@@ -33,13 +34,13 @@ export class ReportNewComponent extends BaseStixComponent implements OnInit {
     }
 
     public saveButtonClicked(): void {
-        let subscription = super.create(this.report).subscribe(
+        const subscription = super.create(this.report).subscribe(
             (data) => {
                 this.saveRelationships(data as Report);
                 this.location.back();
             }, (error) => {
                 // handle errors here
-                 console.log('error ' + error);
+                console.log('error ' + error);
             }, () => {
                 // prevent memory links
                 if (subscription) {
@@ -51,7 +52,7 @@ export class ReportNewComponent extends BaseStixComponent implements OnInit {
 
     private saveRelationships(report: Report): void {
         this.attackPatterns.forEach((relatedRecord) => {
-            let relationship = new Relationship();
+            const relationship = new Relationship();
             relationship.attributes.relationship_type = 'uses';
             relationship.attributes.source_ref = report.id;
             relationship.attributes.target_ref = relatedRecord.id;
@@ -59,8 +60,8 @@ export class ReportNewComponent extends BaseStixComponent implements OnInit {
         });
 
         this.intrusionSets.forEach((relatedRecord) => {
-            let relationship = new Relationship();
-            relationship.attributes.relationship_type =  'attributed-to';
+            const relationship = new Relationship();
+            relationship.attributes.relationship_type = 'attributed-to';
             relationship.attributes.source_ref = report.id;
             relationship.attributes.target_ref = relatedRecord.id;
             this.saveRelationship(relationship);
@@ -68,11 +69,11 @@ export class ReportNewComponent extends BaseStixComponent implements OnInit {
     }
 
     private saveRelationship(relationship: Relationship): void {
-        let created = new Date();
+        const created = new Date();
         relationship.attributes.created = created;
         relationship.attributes.modified = created;
         relationship.attributes.version = '1';
-        let subscription = super.create(relationship).subscribe(
+        const subscription = super.create(relationship).subscribe(
             (data) => {
                 console.log('saved');
             }, (error) => {
@@ -91,7 +92,7 @@ export class ReportNewComponent extends BaseStixComponent implements OnInit {
     private add(event: any): void {
         if (event.type === 'attack-patterns' && !this.found(this.attackPatterns, event)) {
             this.attackPatterns.push(event as AttackPattern);
-        } else if (event.type === 'intrusion-sets'  && !this.found(this.intrusionSets, event)) {
+        } else if (event.type === 'intrusion-sets' && !this.found(this.intrusionSets, event)) {
             this.intrusionSets.push(event as IntrusionSet);
         }
     }
@@ -102,12 +103,12 @@ export class ReportNewComponent extends BaseStixComponent implements OnInit {
         if (object.type === 'attack-patterns') {
             this.attackPatterns = this.attackPatterns.filter((o) => o.id !== object.id);
         } else if (object.type === 'intrusion-sets') {
-            this.intrusionSets = this.intrusionSets.filter((o) =>  o.id !== object.id );
+            this.intrusionSets = this.intrusionSets.filter((o) => o.id !== object.id);
         }
     }
 
     private found(list: any[], object: any): any {
-        return list.find( (entry) => { return entry.id === object.id; } );
+        return list.find((entry) => entry.id === object.id);
     }
 
     private dateChange(date: string): void {
