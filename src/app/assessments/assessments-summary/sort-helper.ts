e51e4299badc789afc3bb4886f93f8eb00864bd3
mushinlogit <mushinlogit@gmail.com>
2017-09-25 16:02:56 -0400

    345 threat boundries creation page, no backend

--- src/app/assessments/assessments-summary/sort-helper.ts
+++ src/app/assessments/assessments-summary/sort-helper.ts
@@ -4,6 +4,44 @@ import { AttackPattern } from '../../models/attack-pattern';
 
 export class SortHelper {
 
+    public static sortDescByField<T>(field: string): (a: T, b: T) => number {
+        if (!field) {
+            throw new Error('please provide a sort field');
+        }
+        
+        const sorter = (a: T, b: T) => {
+            const val1: T = a[field];
+            const val2: T = b[field];
+            if (val1 > val2) {
+                return 1;
+            } else if (val1 < val2) {
+                return -1;
+            }
+            return 0;
+        };
+
+        return sorter;
+    }
+
+    public static sortAscByField<T>(field: string): (a: T, b: T) => number {
+        if (!field) {
+            throw new Error('please provide a sort field');
+        }
+        
+        const sorter = (a: T, b: T) => {
+            const val1: T = a[field];
+            const val2: T = b[field];
+            if (val1 > val2) {
+                return -1;
+            } else if (val1 < val2) {
+                return 1;
+            }
+            return 0;
+        };
+
+        return sorter;
+    }
+
     public static sortDesc(): (a: any, b: any) => number {
         const sorter = (a: any, b: any) => {
             if (a > b) {
