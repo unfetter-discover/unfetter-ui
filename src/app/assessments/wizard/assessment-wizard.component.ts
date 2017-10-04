a3136e60a70bd1b49f89034a5147b06a55ef06f3
j987987 <jason@purplelabs.io>
2017-10-04 10:14:39 -0400

    fixed assessments wizard headings and descriptions, added media-query based flexbox helpers #484

--- src/app/assessments/wizard/assessment-wizard.component.ts
+++ src/app/assessments/wizard/assessment-wizard.component.ts
@@ -408,10 +408,10 @@ export class AssessmentComponent extends Measurements implements OnInit, OnDestr
       const groupingStages = stixObject.groupings;
       if (!groupingStages) {
         const phaseName = 'unknown';
-        if (!groupings[phaseName]) {
-          const description = 'unknown description';
-          groupings[phaseName] = description;
-        }
+        // if (!groupings[phaseName]) {
+        //   const description = 'unknown description';
+        //   groupings[phaseName] = description;
+        // }
       } else {
         groupingStages.forEach((groupingStage) => {
           const phaseName = groupingStage.groupingValue;
@@ -419,9 +419,10 @@ export class AssessmentComponent extends Measurements implements OnInit, OnDestr
             const description = groupingStage.description;
             if (description) {
               groupings[phaseName] = description;
-            } else {
-              groupings[phaseName] = phaseName;
-            }
+            } 
+            // else {
+            //   groupings[phaseName] = phaseName;
+            // }
           }
         });
       }
