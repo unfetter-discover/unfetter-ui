f48a42d6eb075b3d738e8f8fc565d7dbe85fba27
j987987 <jason@purplelabs.io>
2017-10-11 10:41:20 -0400

    refactored various services to use generic api for auth

--- src/app/components/base-service.component.ts
+++ src/app/components/base-service.component.ts
@@ -1,46 +1,69 @@
 import { Injectable } from '@angular/core';
-import { Headers, Http, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
+
+import { GenericApi } from '../global/services/genericapi.service';
 
 @Injectable()
 export class BaseComponentService {
 
-  private headers = new Headers({ 'Content-Type': 'application/json' });
+  constructor(private genericApi: GenericApi) { }
 
-  constructor(private http: Http) { }
-
-  public autoCompelet(url: string): Observable<any[]> {
-
-    return this.http
-      .get(url)
-      .map((response) => {
-        return response.json().data as any[];
-      });
+  public autoCompelet(url: string): Observable<any> {
+    return this.genericApi.get(url);
   }
 
-  public get(url: string): Observable<any[]> {
-    return this.http
-      .get(url)
-      .map((response) => {
-        return response.json().data as any[];
-      });
+  public get(url: string): Observable<any> {
+    return this.genericApi.get(url);
   }
 
-  public delete(url: string, id: string): Observable<any[]> {
+  public delete(url: string, id: string): Observable<any> {
     const uri = `${url}/${id}`;
-    return this.http
-      .delete(uri, { headers: this.headers })
-      .map((response) => {
-        return response.json();
-      });
+    return this.genericApi.delete(url);
   }
 
-  public save(url: string, item: any): Observable<any[]> {
-    return this.http
-      .post(url, JSON.stringify({ data: item }), { headers: this.headers })
-      .map((response) => {
-        return response.json().data;
-      });
+  public save(url: string, item: any): Observable<any> {
+    return this.genericApi.post(url, { data: item });
   }
 }
+
+// @Injectable()
+// export class BaseComponentService {
+
+//   private headers = new Headers({ 'Content-Type': 'application/json' });
+
+//   constructor(private http: Http) { }
+
+//   public autoCompelet(url: string): Observable<any[]> {
+
+//     return this.http
+//       .get(url)
+//       .map((response) => {
+//         return response.json().data as any[];
+//       });
+//   }
+
+//   public get(url: string): Observable<any[]> {
+//     return this.http
+//       .get(url)
+//       .map((response) => {
+//         return response.json().data as any[];
+//       });
+//   }
+
+//   public delete(url: string, id: string): Observable<any[]> {
+//     const uri = `${url}/${id}`;
+//     return this.http
+//       .delete(uri, { headers: this.headers })
+//       .map((response) => {
+//         return response.json();
+//       });
+//   }
+
+//   public save(url: string, item: any): Observable<any[]> {
+//     return this.http
+//       .post(url, JSON.stringify({ data: item }), { headers: this.headers })
+//       .map((response) => {
+//         return response.json().data;
+//       });
+//   }
+// }
