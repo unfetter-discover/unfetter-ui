2339321ee7e53376e3f52c4310546de58c82d1fd
j987987 <jason@purplelabs.io>
2017-10-13 15:31:55 -0400

    identity added to user object

--- src/app/users/register/register.component.ts
+++ src/app/users/register/register.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 
 import { UsersService } from '../users.service';
 import { AuthService } from '../../global/services/auth.service';
+import { Constance } from '../../utils/constance';
 
 @Component({
     selector: 'register',
@@ -17,6 +18,9 @@ export class RegisterComponent implements OnInit {
     public registrationSubmitted: boolean = false;
     public submitError: boolean = false;
 
+    public identityClasses = Constance.IDENTITY_CLASSES;
+    public identitySectors = Constance.IDENTITY_SECTOR_OPTIONS;
+
     constructor(private usersService: UsersService, private router: Router, private authService: AuthService) {  }
 
     public ngOnInit() {
@@ -26,7 +30,6 @@ export class RegisterComponent implements OnInit {
                 .subscribe(
                     (user) => {
                         this.userReturn = user = user.attributes;
-                        console.log(this.userReturn);
                         
                         this.form = new FormGroup({
                             firstName: new FormControl(user.firstName ? user.firstName : '', Validators.required),
@@ -36,8 +39,15 @@ export class RegisterComponent implements OnInit {
                                 Validators.required,
                                 Validators.email
                             ]),
-                            organizations: new FormControl(user.organizations ? user.organizations : [''])
-                        });                      
+                            // organizations: new FormControl(user.organizations ? user.organizations : ['']),
+                            identity: new FormGroup({
+                                name: new FormControl('', Validators.required),
+                                description: new FormControl(''),
+                                identity_class: new FormControl('', Validators.required),
+                                sectors: new FormControl(['']),
+                                contact_information: new FormControl(''),
+                            })
+                        }); 
                     },
                     (err) => {
                         console.log(err);                        
@@ -54,7 +64,9 @@ export class RegisterComponent implements OnInit {
     public registerSubmit() {
         this.registrationSubmitted = true;
         for (let control in this.form.controls) {
-            if (this.form.controls[control].value && this.form.controls[control].value !== '') {
+            if (control === 'identity') {
+                continue;
+            } else if (this.form.controls[control].value && this.form.controls[control].value !== '') {
                 if (this.form.controls[control].value instanceof Array) {                    
                     let validValues = this.form.controls[control].value.filter((el) => el && el !== '');
                     if (validValues && validValues.length) {
@@ -64,7 +76,25 @@ export class RegisterComponent implements OnInit {
                     this.userReturn[control] = this.form.controls[control].value;
                 }
             }            
-        }        
+        }  
+
+        let identity = this.form.get('identity').value;           
+        let temp = {};
+
+        for (let control in identity) {
+            if (identity[control] && identity[control] !== '') {
+                if (identity[control] instanceof Array) {
+                    let validValues = identity[control].filter((el) => el && el !== '');
+                    if (validValues && validValues.length) {
+                        temp[control] = validValues;
+                    }
+                } else {
+                    temp[control] = identity[control];
+                }
+            }
+        } 
+        this.userReturn.identity = temp;
+             
         let submitRegistration$ = this.usersService.finalizeRegistration(this.userReturn)
             .subscribe(
                 (res) => {
