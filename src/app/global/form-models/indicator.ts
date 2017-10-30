feb72528ebd3806cdf00387ab5d723e9b0f65392
j987987 <jason@purplelabs.io>
2017-10-30 16:03:19 -0400

    fixes #512, fixes #513, general UX improvements to add indicators form (#127)
    * fixed formarray issues, added remove to chips, added mat well
    
    * changed collapsible trigger styling, added date range validaiton in add indicators
    
    * fixes #512, fixes #513, general UX improvements to add indicators form
    
    * removed pattern_lang reference
    
    * added loading spinner to analytic hub

--- src/app/global/form-models/indicator.ts
+++ src/app/global/form-models/indicator.ts
@@ -4,7 +4,6 @@ export const IndicatorForm  = () => new FormGroup({
     name: new FormControl('', Validators.required),
     description: new FormControl(),
     labels: new FormArray([]),
-    pattern_lang: new FormControl(),
     pattern: new FormControl('', Validators.required),
     valid_from: new FormControl(new Date(), Validators.required),
     valid_until: new FormControl(new Date()),
--- src/app/indicator-sharing/add-indicator/add-indicator.component.ts
+++ src/app/indicator-sharing/add-indicator/add-indicator.component.ts
@@ -22,8 +22,8 @@ import { MatDialogRef } from '@angular/material';
 export class AddIndicatorComponent implements OnInit {
 
     public form: FormGroup | any;
-    public showExternalReferences: boolean = true;
-    public showKillChainPhases: boolean = true;
+    public showExternalReferences: boolean = false;
+    public showKillChainPhases: boolean = false;
 
     constructor(public dialogRef: MatDialogRef<any>, private indicatorSharingService: IndicatorSharingService) { }    
 
@@ -42,8 +42,7 @@ export class AddIndicatorComponent implements OnInit {
         const tempIndicator = this.buildIndicator({}, this.form.value);
         const addIndicator$ = this.indicatorSharingService.addIndicator(tempIndicator)
             .subscribe(
-                (res) => {
-                    console.log(res);                    
+                (res) => {                   
                     this.resetForm();
                     this.dialogRef.close(true);
                 },
@@ -76,7 +75,6 @@ export class AddIndicatorComponent implements OnInit {
             } else {
                 switch ((typeof obj[prop])) {
                     case 'object':
-                        console.log(obj[prop]);
                         if (obj[prop] instanceof Date) {
                             tempIndicator[prop] = obj[prop];
                         } else if (obj[prop] && Object.keys(obj[prop]).length > 0) {
