f8c546eaf295b3b23bfc955499d9fea1da383f87
mushinlogit <mushinlogit@gmail.com>
2017-11-03 16:46:12 -0400

    fixup

--- src/app/threat-report-overview/add-external-report/external-report-form.ts
+++ src/app/threat-report-overview/add-external-report/external-report-form.ts
@@ -9,25 +9,7 @@ export const ExternalReportForm = () => new FormGroup({
     name: new FormControl('', Validators.required),
     description: new FormControl(),
     granular_markings: new FormArray([]),
-    // external_references: new FormArray([]),
+    external_references: new FormArray([]),
     kill_chain_phases: new FormArray([]),
     object_refs: new FormArray([])
-    // title: new FormControl('', Validators.required),
-    // source_type: new FormControl('', Validators.required),
-    // alaStage: new FormControl(),
-    // afaObjective: new FormControl(),
-    // afaAction: new FormControl(),
-    // description: new FormControl(),
-    // actionParagraph: new FormControl(),
-    // actionClassification: new FormControl(),
-    // reportClassification: new FormControl(),
-    // reportDtg: new FormControl(),
-    // declassification: new FormControl(),
-    // ala: new FormControl(),
-    // ama: new FormControl(),
-    // afa: new FormControl(),
-    // author: new FormControl(),
-    // url: new FormControl()
 });
-// Report Id,Title,Source Type,ALA Stage,AFA Objective,AFA Action,Description,Action Paragraph,Action Classification,Report 
-//     Classification,Report DTG,Declassification,ALA,AMA,AFA,AUTHOR,ADDED DTG,QC AUTHOR,QC DTG
