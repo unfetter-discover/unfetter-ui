87661c3ca3d13dd91fbc6d9651ec59b912eb95c7
Alchemist <infosec-alchemist@users.noreply.github.com>
2017-11-17 11:03:24 -0500

    Fixed spelling issues. #Fixes unfetter-discover/unfetter:564 (#152)

--- src/app/settings/stix-objects/intrusion-sets/intrusion-set-home.component.ts
+++ src/app/settings/stix-objects/intrusion-sets/intrusion-set-home.component.ts
@@ -10,8 +10,8 @@ export class IntrusionSetHomeComponent {
     public pageTitle = 'Intrusion Sets';
     public pageIcon = 'assets/icon/stix-icons/svg/threat-actor-b.svg';
     public description = 'An Intrusion Set is a grouped set of adversarial behaviors and resources with common properties' +
-            'that is believed to believedorchestrated by a single organization.  An Intrusion Set may caputre multiple ' +
-            'Campaigns organizationother activities that are all tied together by shared attributes indicating a commont ' +
-            'known or unknown Threat Actor.  Threat Actors could move from supporting one Intrusion Set to supporting another' +
+            ' that is believed to be orchestrated by a single organization.  An Intrusion Set may capture multiple ' +
+            ' Campaigns organizationother activities that are all tied together by shared attributes indicating a commont' +
+            ' known or unknown Threat Actor.  Threat Actors could move from supporting one Intrusion Set to supporting another' +
             ', or may support multiple at the same time.';
 }
