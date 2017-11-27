012465f8562af1443de2684fcf0c457d1de8c308
J Burns <burnsjennifere@gmail.com>
2017-11-27 14:52:36 -0500

    Feature: Allow edit of config object within admin menu (#164)
    * Added basic component for editing config in DB
    
    * Updated edit components
    
    * Updated config edit to be generalized
    
    * Added capability too create new config objects
    
    * Removed unneeded css
    
    * Fixed spacing typo before PR

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule } from '@angular/material';
-
+import { MatButtonModule, MatInputModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
 import { routing } from './admin-routing.module';
 import { GlobalModule } from '../global/global.module';
 import { AdminService } from './admin.service';
 import { ApproveUsersComponent } from './approve-users/approve-users.component';
 import { AdminLayoutComponent } from './admin-layout/admin-layout.component';
 import { SiteUsageComponent } from './site-usage/site-usage.component';
+import { ConfigEditComponent } from './config-edit/config-edit.component';
 import { OrgLeaderApprovalComponent } from './org-leader-approval/org-leader-approval.component';
 import { ChartsModule } from 'ng2-charts';
 
@@ -16,14 +17,17 @@ import { ChartsModule } from 'ng2-charts';
         CommonModule,
         GlobalModule,
         routing,
+        FormsModule,
         MatButtonModule,
+        MatInputModule,
         ChartsModule
     ],
     declarations: [
         ApproveUsersComponent,
         AdminLayoutComponent,
         SiteUsageComponent,
-        OrgLeaderApprovalComponent
+        OrgLeaderApprovalComponent,
+        ConfigEditComponent
     ],
     providers: [
         AdminService
--- src/app/admin/admin.service.ts
+++ src/app/admin/admin.service.ts
@@ -7,9 +7,10 @@ import { Constance } from '../utils/constance';
 
 @Injectable()
 export class AdminService {
-    
+
     private adminUrl = Constance.ADMIN_URL;
     private identitiesUrl = Constance.IDENTITIES_URL;
+    private configUrl = Constance.CONFIG_URL;
 
     constructor(private genericApi: GenericApi) { }
 
@@ -31,6 +32,26 @@ export class AdminService {
 
     public processUserApproval(user): Observable<any> {
         return this.genericApi.post(`${this.adminUrl}/process-user-approval`, user);
+    }
+
+    public getConfig(): Observable<any> {
+        return this.genericApi.get(`${this.configUrl}`);
+    }
+
+    public getSingleConfig(id): Observable<any> {
+        return this.genericApi.get(`${this.configUrl}/${id}`)
+    }
+
+    public deleteSingleConfig(id): Observable<any> {
+        return this.genericApi.delete(`${this.configUrl}/${id}`)
+    }
+
+    public addConfig(data): Observable<any> {
+        return this.genericApi.post(`${this.configUrl}`, data)
+    }
+
+    public processChangedData(data, id): Observable<any> {
+        return this.genericApi.patch(`${this.configUrl}/${id}`, data);
     }
 
     public getOrganizations(): Observable<any> {
