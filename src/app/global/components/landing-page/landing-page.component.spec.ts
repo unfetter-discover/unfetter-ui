a5e8640a3449f0b40cfa1ba56c36ade33ace7525
j987987 <jason@purplelabs.io>
2018-01-12 14:17:32 -0500

    fixed some unit tests

--- src/app/global/components/landing-page/landing-page.component.spec.ts
+++ src/app/global/components/landing-page/landing-page.component.spec.ts
@@ -1,25 +1,41 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
-// import { LandingPageComponent } from './landing-page.component';
+import { LandingPageComponent } from './landing-page.component';
+import { AuthService } from '../../../core/services/auth.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ConfigService } from '../../../core/services/config.service';
+import { GenericApi } from '../../../core/services/genericapi.service';
 
-// describe('LandingPageComponent', () => {
-//   let component: LandingPageComponent;
-//   let fixture: ComponentFixture<LandingPageComponent>;
+describe('LandingPageComponent', () => {
+  let component: LandingPageComponent;
+  let fixture: ComponentFixture<LandingPageComponent>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ LandingPageComponent ]
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+        imports: [
+            RouterTestingModule,
+            HttpClientTestingModule  
+        ],
+        declarations: [ LandingPageComponent ],
+        schemas: [ NO_ERRORS_SCHEMA ],
+        providers: [ 
+            AuthService,
+            ConfigService,
+            GenericApi
+        ]
+    })
+    .compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(LandingPageComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LandingPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/global/components/notification-window/notification-window.component.spec.ts
+++ src/app/global/components/notification-window/notification-window.component.spec.ts
@@ -1,25 +1,50 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatIconModule } from '@angular/material';
+import { RouterTestingModule } from '@angular/router/testing';
+import { StoreModule } from '@ngrx/store';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
-// import { NotificationWindowComponent } from './notification-window.component';
+import { NotificationWindowComponent } from './notification-window.component';
+import { FieldSortPipe } from '../../pipes/field-sort.pipe';
+import { TimeAgoPipe } from '../../pipes/time-ago.pipe';
+import { GenericApi } from '../../../core/services/genericapi.service';
+import { AuthService } from '../../../core/services/auth.service';
+import { ConfigService } from '../../../core/services/config.service';
+import { reducers } from '../../../root-store/app.reducers';
 
-// describe('NotificationWindowComponent', () => {
-//   let component: NotificationWindowComponent;
-//   let fixture: ComponentFixture<NotificationWindowComponent>;
+fdescribe('NotificationWindowComponent', () => {
+  let component: NotificationWindowComponent;
+  let fixture: ComponentFixture<NotificationWindowComponent>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ NotificationWindowComponent ]
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ 
+            NotificationWindowComponent,
+            FieldSortPipe,
+            TimeAgoPipe            
+      ],
+      imports: [
+          MatIconModule,
+          RouterTestingModule,
+          HttpClientTestingModule,
+          StoreModule.forRoot(reducers)
+      ],
+      providers: [
+          AuthService,
+          GenericApi,
+          ConfigService
+      ]
+    })
+    .compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(NotificationWindowComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NotificationWindowComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
