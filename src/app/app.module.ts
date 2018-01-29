4f4eceb654f1a2aa242a2cb45eb14bb5f0e2a82f
j987987 <jason@purplelabs.io>
2018-01-29 14:38:08 -0500

    removed unneded imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule, ApplicationRef } from '@angular/core';
 import { RouterModule, PreloadAllModules, Router } from '@angular/router';
-import { MatProgressBarModule, MatCardModule } from '@angular/material';
 import { ComponentModule } from './components/component.module';
 import { StixModule } from './settings/stix.module';
 import { StoreModule } from '@ngrx/store';
@@ -43,7 +42,6 @@ import { NotificationEffects } from './root-store/notification/notification.effe
     NoContentComponent,
   ],
   imports: [ // import Angular's modules
-    MatCardModule,
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
