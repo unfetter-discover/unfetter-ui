247d5497663f29487533f53a9a53cda62b040818
webiator <6414732+webiator@users.noreply.github.com>
2018-02-02 09:54:47 -0500

    Adds report editor dialog. Work products can now be saved. Import behavior only thing missing at this time.

--- src/app/threat-report-overview/editor/threat-report-editor.component.spec.ts
+++ src/app/threat-report-overview/editor/threat-report-editor.component.spec.ts
@@ -2,6 +2,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Location } from '@angular/common';
+import { Observable } from 'rxjs/Observable';
+import * as UUID from 'uuid';
 
 import { FormsModule } from '@angular/forms';
 import {
@@ -17,17 +19,14 @@ import {
         MatTableDataSource,
     } from '@angular/material';
 
-import * as UUID from 'uuid';
-
+import { Report } from '../../models/report';
+import { ThreatReport } from '../models/threat-report.model';
+import { ExternalReference } from '../../models/stix/external_reference';
 import { ThreatReportEditorComponent } from './threat-report-editor.component';
 import { ThreatReportOverviewService } from '../../threat-dashboard/services/threat-report-overview.service';
 import { ThreatReportSharedService } from '../services/threat-report-shared.service';
 import { LoadingSpinnerComponent } from '../../global/components/loading-spinner/loading-spinner.component';
 import { GenericApi } from '../../core/services/genericapi.service';
-import { ThreatReport } from '../models/threat-report.model';
-import { Observable } from 'rxjs/Observable';
-import { Report } from '../../models/report';
-import { ExternalReference } from '../../models/stix/external_reference';
 
 class MockThreatReport {
     public static empty(): ThreatReport {
