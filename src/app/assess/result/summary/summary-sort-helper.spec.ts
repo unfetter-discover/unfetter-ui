f71817ec33bc589e71a95e88f0e0d116a2fec51e
mpmmasterpeace <24652622+mpmmasterpeace@users.noreply.github.com>
2018-02-12 09:38:41 -0500

    wip data in top two boxes now

--- src/app/assess/result/summary/summary-sort-helper.spec.ts
+++ src/app/assess/result/summary/summary-sort-helper.spec.ts
@@ -9,6 +9,17 @@ fdescribe('SummarySortHelper', () => {
     expect(new SummarySortHelper()).toBeTruthy();
   });
 
+  it('should check if the first of two values is unset, but not zero', () => {
+    expect(SummarySortHelper.isFirstValueOnlyUnset(null, null)).toEqual(false);
+    expect(SummarySortHelper.isFirstValueOnlyUnset(null, {})).toEqual(true);
+    expect(SummarySortHelper.isFirstValueOnlyUnset({}, null)).toEqual(false);
+    expect(SummarySortHelper.isFirstValueOnlyUnset({}, {})).toEqual(false);
+    expect(SummarySortHelper.isFirstValueOnlyUnset(0, {})).toEqual(false);
+    expect(SummarySortHelper.isFirstValueOnlyUnset(0, null)).toEqual(false);
+    expect(SummarySortHelper.isFirstValueOnlyUnset(null, 0)).toEqual(true);
+    expect(SummarySortHelper.isFirstValueOnlyUnset({}, 0)).toEqual(false);
+  });
+
   it('should sort killchain risk objects by risk decending', () => {
     let riskObjects: AssessKillChainType[] = [];
     let expected: AssessKillChainType[] = [];
@@ -22,6 +33,11 @@ fdescribe('SummarySortHelper', () => {
 
     riskObjects = [{ risk: 1, questions: null, objects: null, phaseName: null }, { risk: .5, questions: null, objects: null, phaseName: null }];
     expected = [{ risk: 1, questions: null, objects: null, phaseName: null }, { risk: .5, questions: null, objects: null, phaseName: null }];
+    riskObjects.sort(SummarySortHelper.sortByRiskDesc());
+    expect(JSON.stringify(riskObjects)).toEqual(JSON.stringify(expected));
+
+    riskObjects = [{ risk: null, questions: null, objects: null, phaseName: null }, { risk: 0, questions: null, objects: null, phaseName: null }];
+    expected = [{ risk: 0, questions: null, objects: null, phaseName: null }, { risk: null, questions: null, objects: null, phaseName: null }];
     riskObjects.sort(SummarySortHelper.sortByRiskDesc());
     expect(JSON.stringify(riskObjects)).toEqual(JSON.stringify(expected));
 
@@ -64,6 +80,11 @@ fdescribe('SummarySortHelper', () => {
 
     phases = [{ assessedObjects: null, attackPatterns: null, _id: null, avgRisk: undefined }];
     expected = [{ assessedObjects: null, attackPatterns: null, _id: null, avgRisk: undefined }];
+    phases.sort(SummarySortHelper.sortByAvgRiskDesc());
+    expect(JSON.stringify(phases)).toEqual(JSON.stringify(expected));
+
+    phases = [{ assessedObjects: null, attackPatterns: null, _id: null, avgRisk: null }, { assessedObjects: null, attackPatterns: null, _id: null, avgRisk: 0 }];
+    expected = [{ assessedObjects: null, attackPatterns: null, _id: null, avgRisk: 0 }, { assessedObjects: null, attackPatterns: null, _id: null, avgRisk: null }];
     phases.sort(SummarySortHelper.sortByAvgRiskDesc());
     expect(JSON.stringify(phases)).toEqual(JSON.stringify(expected));
 
@@ -187,4 +208,6 @@ fdescribe('SummarySortHelper', () => {
     patterns.sort(SummarySortHelper.sortBySophisticationAsc());
     expect(JSON.stringify(patterns)).toEqual(JSON.stringify(expected));
   });
+
+
 });
--- src/app/assess/result/summary/summary-sort-helper.ts
+++ src/app/assess/result/summary/summary-sort-helper.ts
@@ -3,23 +3,34 @@ import { AssessAttackPattern } from '../../../models/assess/assess-attack-patter
 import { AssessKillChainType } from '../../../models/assess/assess-kill-chain-type';
 
 export class SummarySortHelper {
+
+    public static isFirstValueOnlyUnset(firstValue: any, otherValue: any) {
+        let firstValueOnlyIsUnset = false;
+        if (firstValue !== 0) {
+            if (!firstValue && (otherValue || otherValue === 0)) {
+                firstValueOnlyIsUnset = true;
+            }
+        }
+        return firstValueOnlyIsUnset;
+    }
+
     public static sortByRiskDesc(): (a: AssessKillChainType, b: AssessKillChainType) => number {
         const sorter = (a: AssessKillChainType, b: AssessKillChainType) => {
-            if ((!b.risk && a.risk) || (a.risk > b.risk)) {
+            if (this.isFirstValueOnlyUnset(b.risk, a.risk) || b.risk < a.risk) {
                 return -1;
-            } else if ((!a.risk && b.risk) || (a.risk < b.risk)) {
+            } else if (this.isFirstValueOnlyUnset(a.risk, b.risk) || (a.risk < b.risk)) {
                 return 1;
             }
             return 0;
         };
         return sorter;
     }
-    
+
     public static sortByAvgRiskDesc(): (a: Phase, b: Phase) => number {
         const sorter = (a: Phase, b: Phase) => {
-            if ((!b.avgRisk && a.avgRisk) || (a.avgRisk > b.avgRisk)) {
+            if (this.isFirstValueOnlyUnset(b.avgRisk, a.avgRisk) || b.avgRisk < a.avgRisk) {
                 return -1;
-            } else if ((!a.avgRisk && b.avgRisk) || (a.avgRisk < b.avgRisk)) {
+            } else if (this.isFirstValueOnlyUnset(a.avgRisk, b.avgRisk) || a.avgRisk < b.avgRisk) {
                 return 1;
             }
             return 0;
@@ -29,11 +40,11 @@ export class SummarySortHelper {
 
     public static sortBySophisticationAsc(): (a: AssessAttackPattern, b: AssessAttackPattern) => number {
         const sorter = (a: AssessAttackPattern, b: AssessAttackPattern) => {
-            if ((!a.x_unfetter_sophistication_level && b.x_unfetter_sophistication_level) || 
-                (a.x_unfetter_sophistication_level > b.x_unfetter_sophistication_level)) {
+            if (this.isFirstValueOnlyUnset(a.x_unfetter_sophistication_level, b.x_unfetter_sophistication_level) ||
+                a.x_unfetter_sophistication_level > b.x_unfetter_sophistication_level) {
                 return 1;
-            } else if ((!b.x_unfetter_sophistication_level && a.x_unfetter_sophistication_level) || 
-                (a.x_unfetter_sophistication_level < b.x_unfetter_sophistication_level)) {
+            } else if (this.isFirstValueOnlyUnset(b.x_unfetter_sophistication_level, a.x_unfetter_sophistication_level) ||
+                b.x_unfetter_sophistication_level > a.x_unfetter_sophistication_level) {
                 return -1;
             }
             return 0;
