045a74dc12465fdb0650d1178a592308eb74159b
j987987 <j987897@users.noreply.github.com>
2018-02-28 13:11:50 -0500

    fixes unfetter-discover/unfetter#752, added stix relationship generator, added proper stix bundle export to indicator sharing

---
+++ src/app/global/enums/stix-relationship-types.enum.ts
@@ -0,0 +1,11 @@
+export const enum StixRelationshipTypes {
+    ATTRIBUTED_TO = 'attributed-to',
+    IMPERSONATES = 'impersonates',
+    INDICATES = 'indicates',
+    MITIGATES = 'mitigates',
+    TARGETS = 'targets',
+    USES = 'uses',
+    VARIANT_OF = 'varient-of',
+    X_UNFETTER_CAN_RUN = 'can-run',
+    X_UNFETTER_DETECTS = 'detects'
+}
