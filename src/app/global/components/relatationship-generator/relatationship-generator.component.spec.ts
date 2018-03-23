42a0531d2e821ba37730e04cdd1de8c1b3b25ecc
j987987 <j987897@users.noreply.github.com>
2018-03-23 11:06:01 -0400

    rel gen unit test

--- src/app/global/components/relatationship-generator/relatationship-generator.component.spec.ts
+++ src/app/global/components/relatationship-generator/relatationship-generator.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { MatSelectModule } from '@angular/material';
 import { Observable } from 'rxjs/Observable';
@@ -8,10 +8,15 @@ import { GenericApi } from '../../../core/services/genericapi.service';
 import { CapitalizePipe } from '../../pipes/capitalize.pipe';
 import { FieldSortPipe } from '../../pipes/field-sort.pipe';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { OverlayContainer } from '@angular/cdk/overlay';
+import { By } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-fdescribe('RelatationshipGeneratorComponent', () => {
+describe('RelatationshipGeneratorComponent', () => {
   let component: RelatationshipGeneratorComponent;
   let fixture: ComponentFixture<RelatationshipGeneratorComponent>;
+  let overlayContainerElement: HTMLElement;
+  let selectEl: HTMLInputElement;
 
   const mockModel = {
     attributes: {
@@ -43,12 +48,27 @@ fdescribe('RelatationshipGeneratorComponent', () => {
       ],
       imports: [
         MatSelectModule,
-        NoopAnimationsModule
+        NoopAnimationsModule,
+        FormsModule
       ],
       providers: [
         {
           provide: GenericApi,
           useValue: mockGenericApi
+        },
+        {
+          provide: OverlayContainer, useFactory: () => {
+            overlayContainerElement = document.createElement('div') as HTMLElement;
+            overlayContainerElement.classList.add('cdk-overlay-container');
+
+            document.body.appendChild(overlayContainerElement);
+
+            // remove body padding to keep consistent cross-browser
+            document.body.style.padding = '0';
+            document.body.style.margin = '0';
+
+            return { getContainerElement: () => overlayContainerElement };
+          }
         }
       ],
       schemas: [ NO_ERRORS_SCHEMA ]
@@ -66,4 +86,16 @@ fdescribe('RelatationshipGeneratorComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should update model after making a selection', fakeAsync(() => {
+    fixture.detectChanges();
+    selectEl = fixture.debugElement.query(By.css('.mat-select-trigger')).nativeElement;
+    selectEl.click();
+    fixture.detectChanges();
+    let option = overlayContainerElement.querySelector('mat-option') as HTMLElement;
+    option.click();
+    fixture.detectChanges();
+    tick(500);
+    expect(component.model.attributes.metaProperties.relationships[0]).toEqual('attack-pattern-1234');
+  }));
 });
