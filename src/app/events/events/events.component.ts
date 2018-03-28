019b743bdcad75c03f4bce1863910c5f94fc7974
mpmmasterpeace <24652622+mpmmasterpeace@users.noreply.github.com>
2018-03-28 14:14:44 -0400

    new events dashboard initial check-in; themed and routing properly

---
+++ src/app/events/events/events.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'events',
+  templateUrl: './events.component.html',
+  styleUrls: ['./events.component.scss']
+})
+export class EventsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
