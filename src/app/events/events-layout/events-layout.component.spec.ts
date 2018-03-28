81e145e606cb3c57b3aa75450f6a0dc62234e81a
mpmmasterpeace <24652622+mpmmasterpeace@users.noreply.github.com>
2018-03-28 14:38:27 -0400

    fix broken tests

--- src/app/events/events-layout/events-layout.component.spec.ts
+++ src/app/events/events-layout/events-layout.component.spec.ts
@@ -3,18 +3,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { EventsLayoutComponent } from './events-layout.component';
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { RouterTestingModule } from '@angular/router/testing';
 =======
 >>>>>>> new events dashboard initial check-in; themed and routing properly
 =======
 >>>>>>> new events dashboard initial check-in; themed and routing properly
+=======
+import { RouterTestingModule } from '@angular/router/testing';
+>>>>>>> fix broken tests
 
-describe('EventsLayoutComponent', () => {
+fdescribe('EventsLayoutComponent', () => {
   let component: EventsLayoutComponent;
   let fixture: ComponentFixture<EventsLayoutComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
       declarations: [ EventsLayoutComponent ],
@@ -25,6 +30,10 @@ describe('EventsLayoutComponent', () => {
 =======
       declarations: [ EventsLayoutComponent ]
 >>>>>>> new events dashboard initial check-in; themed and routing properly
+=======
+      declarations: [ EventsLayoutComponent ],
+      imports: [ RouterTestingModule ],
+>>>>>>> fix broken tests
     })
     .compileComponents();
   }));
