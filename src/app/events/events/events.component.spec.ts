867c57d920a750228ed20c5530a3eb46bcc1ca2c
mpmmasterpeace <24652622+mpmmasterpeace@users.noreply.github.com>
2018-03-28 14:14:44 -0400

    new events dashboard initial check-in; themed and routing properly

--- src/app/events/events/events.component.spec.ts
+++ src/app/events/events/events.component.spec.ts
@@ -2,10 +2,13 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EventsComponent } from './events.component';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { RouterTestingModule } from '@angular/router/testing';
 import { MatSidenavModule } from '@angular/material';
 import { GlobalModule } from '../../global/global.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+=======
+>>>>>>> new events dashboard initial check-in; themed and routing properly
 =======
 >>>>>>> new events dashboard initial check-in; themed and routing properly
 
@@ -13,6 +16,7 @@ describe('EventsComponent', () => {
   let component: EventsComponent;
   let fixture: ComponentFixture<EventsComponent>;
 
+<<<<<<< HEAD
 <<<<<<< HEAD
   const materialModules = [
     MatSidenavModule
@@ -27,6 +31,11 @@ describe('EventsComponent', () => {
         MatSidenavModule,
         GlobalModule,
       ],
+=======
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ EventsComponent ]
+>>>>>>> new events dashboard initial check-in; themed and routing properly
 =======
   beforeEach(async(() => {
     TestBed.configureTestingModule({
