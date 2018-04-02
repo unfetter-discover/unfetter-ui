80e0fba8d82032990b9239a435ee657d7a74356a
j987987 <j987897@users.noreply.github.com>
2018-04-02 13:40:28 -0400

    infinite scrolling AE

--- src/app/global/global.module.ts
+++ src/app/global/global.module.ts
@@ -31,7 +31,7 @@ import { SophisticationPipe } from './pipes/sophistication.pipe';
 import { FieldSortPipe } from './pipes/field-sort.pipe';
 import { TimeAgoPipe } from './pipes/time-ago.pipe';
 
-// Components
+// Components and directives
 import { RiskIconComponent } from './components/risk-icon/risk-icon.component';
 import { StixTableComponent } from './components/stix-table/stix-table.component';
 import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
@@ -59,6 +59,7 @@ import { TreemapComponent } from './components/treemap/treemap.component';
 import { HeatmapComponent } from './components/heatmap/heatmap.component';
 import { CreatedByRefComponent } from './components/created-by-ref/created-by-ref.component';
 import { RelatationshipGeneratorComponent } from './components/relatationship-generator/relatationship-generator.component';
+import { InfiniteScrollDirective } from './directives/infinite-scroll.directive';
 
 const matModules = [
     MatAutocompleteModule,
@@ -115,6 +116,7 @@ const unfetterComponents = [
     HeatmapComponent,
     CreatedByRefComponent,
     RelatationshipGeneratorComponent,
+    InfiniteScrollDirective,
 ];
 
 @NgModule({
