9f0d91401982f407b8a576c3afd2e9b12a62ad4d
mpmmasterpeace <24652622+mpmmasterpeace@users.noreply.github.com>
2018-04-03 07:25:03 -0400

    Issue 913 (#354)
    * traffic detail table with hard coded data
    
    * added functionality to Traffic Detail table and hard-coded data
    
    * fix test
    
    * traffic detail table with hard coded data
    
    * added functionality to Traffic Detail table and hard-coded data
    
    * fix test
    
    * fix sizing issues
    
    * traffic detail table with hard coded data
    
    * added functionality to Traffic Detail table and hard-coded data
    
    * fix test
    
    * traffic detail table with hard coded data
    
    * added functionality to Traffic Detail table and hard-coded data
    
    * fix sizing issues
    
    * Fix route refresh (#353)
    
    * lazy load stix module
    
    * fixed tests
    
    * development reload updates, try to rehydrate the ngx store
    
    * dialog size
    
    * fix live reload, hotmodule reload is wip
    
    * clean up
    
    * fixed issue with rerotue on login
    
    * traffic detail table with hard coded data
    
    * added functionality to Traffic Detail table and hard-coded data
    
    * fix test
    
    * traffic detail table with hard coded data
    
    * added functionality to Traffic Detail table and hard-coded data
    
    * fix sizing issues
    
    * traffic detail table with hard coded data
    
    * traffic detail table with hard coded data
    
    * added functionality to Traffic Detail table and hard-coded data
    
    * added functionality to Traffic Detail table and hard-coded data
    
    * traffic detail table with hard coded data
    
    * added functionality to Traffic Detail table and hard-coded data
    
    * fix test
    
    * traffic detail table with hard coded data
    
    * added functionality to Traffic Detail table and hard-coded data
    
    * fix sizing issues
    
    * traffic detail table with hard coded data
    
    * traffic detail table with hard coded data
    
    * added functionality to Traffic Detail table and hard-coded data
    
    * added functionality to Traffic Detail table and hard-coded data
    
    * traffic detail table with hard coded data
    
    * added functionality to Traffic Detail table and hard-coded data
    
    * added functionality to Traffic Detail table and hard-coded data
    
    * traffic detail table with hard coded data
    
    * traffic detail table with hard coded data
    
    * added functionality to Traffic Detail table and hard-coded data
    
    * added functionality to Traffic Detail table and hard-coded data

--- src/app/events/events-content/events-content.component.ts
+++ src/app/events/events-content/events-content.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Sighting } from '../../models';
 
 @Component({
   selector: 'events-content',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventsContentComponent implements OnInit {
 
-  constructor() { }
+  readonly recentSightings: Sighting[];
+  readonly columnIds: string[];
 
-  ngOnInit() {
+  constructor() {
+    this.columnIds = ['date', 'ip', 'city', 'country', 'attack_pattern', 'potential actor'];
+    this.recentSightings = [];
   }
 
+  ngOnInit() {
+    // dummy data
+    for (let i = 0; i < 50; i++) {
+      const sighting = new Sighting();
+      // sighting.attributes.sighting_of_ref is some id
+      sighting.attributes['sighting_of_ref_name'] = 'Commonly Used Port';
+      // sighting.attributes.observed_data_refs[] holds ids
+      sighting.attributes.observed_data_refs.push('123.23.2340');
+      sighting.attributes['observed_data_refs_city'] = 'Kyiv';
+      sighting.attributes['observed_data_refs_country'] = 'UA';
+      sighting.attributes['sighting_of_ref_name_intrusion_set_group'] = 'APT3';
+      this.recentSightings.push(sighting);
+    }
+
+  }
 }
