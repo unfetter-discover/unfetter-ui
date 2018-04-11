1006f45506a476038bcbf21676499e1750c10f13
j987987 <j987897@users.noreply.github.com>
2018-04-11 11:43:32 -0400

    refactored relationship array unwrappoing, added intrusion sets by attack pattern

--- src/app/global/static/rxjs-helpers.spec.ts
+++ src/app/global/static/rxjs-helpers.spec.ts
@@ -17,4 +17,28 @@ describe('RxjsHelpers class', () => {
         });
     });
 
+    describe('relationshipArrayToObject function', () => {
+        const relArr = [
+            {
+                _id: '1234',
+                mockData: [1, 2, 3]
+            },
+            {
+                _id: '4567',
+                mockData: [4, 5]
+            }
+        ]
+
+        it('should transform relationship array to object', () => {
+            const relObj = RxjsHelpers.relationshipArrayToObject(relArr, 'mockData');
+            expect(relObj['1234'].length).toBe(3);
+            expect(relObj['4567'].length).toBe(2);
+        });
+
+        it('should return undefined when given wrong property', () => {
+            const relObj = RxjsHelpers.relationshipArrayToObject(relArr, 'wrongProperty');
+            expect(relObj['1234']).toBe(undefined);
+            expect(relObj['4567']).toBe(undefined);
+        });
+    });
 });
