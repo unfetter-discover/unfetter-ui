8c9a50d9e94f7f9b58192d7f36d75c0513821685
mushinlogit <mushinlogit@protonmail.com>
2018-04-13 15:49:51 -0400

    url endpoint fix

--- src/app/utils/constance.ts
+++ src/app/utils/constance.ts
@@ -91,10 +91,10 @@ export const Constance = {
   X_UNFETTER_ASSESSMENT3_URL: 'api/v3/x-unfetter-object-assessments',
   X_UNFETTER_ASSESSMENT3_TYPE: 'x-unfetter-object-assessment',
 
-  X_UNFETTER_CAPABILITY_URL: 'api/v3/x-unfetter-capability',
+  X_UNFETTER_CAPABILITY_URL: 'api/v3/x-unfetter-capabilities',
   X_UNFETTER_CAPABILITY_TYPE: 'x-unfetter-capability',
 
-  X_UNFETTER_CATEGORY_URL: 'api/v3/x-unfetter-category',
+  X_UNFETTER_CATEGORY_URL: 'api/v3/x-unfetter-categories',
   X_UNFETTER_CATEGORY_TYPE: 'x-unfetter-category',
 
   AUTH_URL: 'api/auth',
