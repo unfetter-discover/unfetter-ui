c4d074197ffa081795eb23548b4f0518bfc9ab99
j987987 <j987897@users.noreply.github.com>
2018-04-19 12:34:56 -0400

    normalizeQuotes

--- src/app/global/static/format-helpers.spec.ts
+++ src/app/global/static/format-helpers.spec.ts
@@ -1,10 +1,19 @@
 import { FormatHelpers } from './format-helpers';
 
 describe('format helpers', () => {
-    it('should replace newlines with html breaks', () => {
-        const original = 'text\ntext';
-        const formatted = FormatHelpers.whitespaceToBreak(original);
-            expect(formatted).toEqual('text<br>text');
+    describe('whitespaceToBreak', () => {
+        it('should replace newlines with html breaks', () => {
+            const original = 'text\ntext';
+            const formatted = FormatHelpers.whitespaceToBreak(original);
+            expect(formatted).toBe('text<br>text');
+        });
+    });
+
+    describe('normalizeQuotes', () => {
+        it('should remove non-standard quotes', () => {
+            const original = '‘single’-“double”';
+            const formatted = FormatHelpers.normalizeQuotes(original);
+            expect(formatted).toBe(`'single'-"double"`);
+        });
     });
 });
-
