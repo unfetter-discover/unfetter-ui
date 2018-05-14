fe5ee3b515481e4c104f85e4ac670b93c9354778
mushinlogit <mushinlogit@protonmail.com>
2018-05-14 10:41:44 -0400

    support unfetter-discover/unfetter#1044 fix test disconnects

--- karma.conf.js
+++ karma.conf.js
@@ -27,7 +27,21 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
+    browsers: ['Chrome', 'ChromeHeadlessCustom', 'ChromeHeadless'],
+    singleRun: false,
+    customLaunchers: {
+      'ChromeHeadlessCustom': {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox',
+          '--headless',
+          '--enable-gpu',
+          '--disable-translate',
+          '--disable-extensions'
+        ]
+      }
+    },
+    browserNoActivityTimeout: 20000,
+    browserDisconnectTolerance: 3
   });
 };
