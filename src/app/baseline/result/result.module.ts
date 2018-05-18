c01f72c066c89be250c45d004e5c6800d6706638
sbertrand <sbertrand@mitre.org>
2018-05-18 11:51:56 -0400

    Removed full component from baseline results

--- src/app/baseline/result/result.module.ts
+++ src/app/baseline/result/result.module.ts
@@ -1,35 +1,25 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatCardModule, MatProgressSpinnerModule, MatSelectModule, MatSliderModule, MatTableModule, MatTabsModule } from '@angular/material';
 import { RouterModule } from '@angular/router';
-import { MatProgressSpinnerModule, MatTabsModule, MatCardModule, MatSliderModule, MatTableModule, MatSelectModule } from '@angular/material';
-
-import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
-
+import { StoreModule } from '@ngrx/store';
 import { ChartsModule } from 'ng2-charts';
-
 import { GlobalModule } from '../../global/global.module';
-
-import { SummaryEffects } from './store/summary.effects';
-import { summaryReducer } from './store/summary.reducers';
+import { ResultHeaderComponent } from './result-header/result-header.component';
 import { RiskByAttackPatternEffects } from './store/riskbyattackpattern.effects';
 import { riskByAttackPatternReducer } from './store/riskbyattackpattern.reducers';
-import { SummaryComponent } from './summary/summary.component';
-import { FullComponent } from './full/full.component';
-import { ResultHeaderComponent } from './result-header/result-header.component';
+import { SummaryEffects } from './store/summary.effects';
+import { summaryReducer } from './store/summary.reducers';
+import { SummaryCalculationService } from './summary/summary-calculation.service';
 import { SummaryHeaderComponent } from './summary/summary-header/summary-header.component';
+import { BaselineChartComponent } from './summary/summary-report/baseline-chart/baseline-chart.component';
+import { SophisticationBreakdownComponent } from './summary/summary-report/sophistication-breakdown/sophistication-breakdown.component';
 import { SummaryReportComponent } from './summary/summary-report/summary-report.component';
 import { SummaryTacticsComponent } from './summary/summary-report/summary-tactics/summary-tactics.component';
-import { AssessmentsSummaryComponent } from '../../baselines/baselines-summary/baselines-summary.component';
-import { FullResultEffects } from './store/full-result.effects';
-import { fullAssessmentResultReducer } from './store/full-result.reducers';
-import { BaselineGroupComponent } from './full/group/baselines-group.component';
-import { AddAssessedObjectComponent } from './full/group/add-assessed-object/add-assessed-object.component';
-import { SummaryCalculationService } from './summary/summary-calculation.service';
-import { SophisticationBreakdownComponent } from './summary/summary-report/sophistication-breakdown/sophistication-breakdown.component';
 import { TechniquesChartComponent } from './summary/summary-report/techniques-chart/techniques-chart.component';
-import { BaselineChartComponent } from './summary/summary-report/baseline-chart/baseline-chart.component';
+import { SummaryComponent } from './summary/summary.component';
 
 const materialModules = [
   MatCardModule,
@@ -41,10 +31,7 @@ const materialModules = [
 ];
 
 const moduleComponents = [
-  BaselineGroupComponent,
-  AddAssessedObjectComponent,
   SummaryComponent,
-  FullComponent,
   ResultHeaderComponent,
   SummaryHeaderComponent,
   SummaryReportComponent,
@@ -60,14 +47,12 @@ const moduleComponents = [
     GlobalModule,
     ChartsModule,
     StoreModule.forFeature('summary', summaryReducer),
-    StoreModule.forFeature('fullAssessment', fullAssessmentResultReducer),
     StoreModule.forFeature('riskByAttackPattern', riskByAttackPatternReducer),
-    EffectsModule.forFeature([SummaryEffects, FullResultEffects, RiskByAttackPatternEffects]),
+    EffectsModule.forFeature([SummaryEffects, RiskByAttackPatternEffects]),
   ],
   declarations: [
     ...moduleComponents,
     SummaryComponent,
-    FullComponent,
     ResultHeaderComponent,
     SummaryHeaderComponent,
     SummaryReportComponent,
