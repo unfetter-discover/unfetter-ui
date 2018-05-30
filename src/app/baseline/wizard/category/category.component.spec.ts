2fdbd407f9c7e85c1fb306af0f40680539c708fa
srbennett29 <srb@mitre.org>
2018-05-30 16:17:00 -0400

    added support for adding categories (#473)

--- src/app/baseline/wizard/category/category.component.spec.ts
+++ src/app/baseline/wizard/category/category.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { MatButtonModule, MatCardModule, MatIconModule, MatSelectModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 import { StoreModule, combineReducers } from '@ngrx/store';
@@ -22,6 +23,7 @@ describe('CategoryComponent', () => {
     TestBed.configureTestingModule({
       declarations: [CategoryComponent],
       imports: [
+        FormsModule,
         RouterTestingModule,
         NoopAnimationsModule,
         ...matModules,
