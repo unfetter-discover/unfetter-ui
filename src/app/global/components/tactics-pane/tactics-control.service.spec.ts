d989807f4ebe07ae93694c51bc474710522f601e
webiator <canderson1@keywcorp.com>
2018-05-31 09:25:34 -0400

    Tactics pane unit and coverage tests (#477)
    * Adds new test for configuration store
    
    * Adds more coverage tests for tactics-pane and subcomponents
    
    * Adds tests for components that use the tactics pane component

--- src/app/global/components/tactics-pane/tactics-control.service.spec.ts
+++ src/app/global/components/tactics-pane/tactics-control.service.spec.ts
@@ -14,4 +14,18 @@ describe('TacticControlService', () => {
         expect(service).toBeTruthy();
     }));
 
+    it('should handle change events', inject([TacticsControlService], (service: TacticsControlService) => {
+        expect(service.state.test).toBeUndefined();
+        const sub$ = service.change.subscribe(
+            (ev) => service.state.test = ev,
+            (err) => {},
+            () => sub$ && sub$.unsubscribe()
+        );
+        service.onChange(undefined);
+        expect(service.state.test).toBeUndefined();
+        service.onChange({value: true});
+        expect(service.state.test).toBeDefined();
+        expect(service.state.test.value).toBe(true);
+    }));
+
 });
--- src/app/threat-dashboard/threat-tactics/threat-tactics.component.spec.ts
+++ src/app/threat-dashboard/threat-tactics/threat-tactics.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { StoreModule } from '@ngrx/store';
+import { RouterTestingModule } from '@angular/router/testing';
+import { StoreModule, Store } from '@ngrx/store';
 
 import {
     MatButtonToggleModule,
@@ -13,24 +14,42 @@ import { Carousel } from 'primeng/primeng';
 
 import { ThreatTacticsComponent } from './threat-tactics.component';
 import { TacticsPaneComponent } from '../../global/components/tactics-pane/tactics-pane.component';
-import { TacticsHeatmapComponent } from '../../global/components/tactics-pane/tactics-heatmap/tactics-heatmap.component';
-import { TacticsTreemapComponent } from '../../global/components/tactics-pane/tactics-treemap/tactics-treemap.component';
-import { TacticsCarouselComponent } from '../../global/components/tactics-pane/tactics-carousel/tactics-carousel.component';
-import { TacticsCarouselControlComponent } from '../../global/components/tactics-pane/tactics-carousel/tactics-carousel-control.component';
-import { TacticsTooltipComponent } from '../../global/components/tactics-pane/tactics-tooltip/tactics-tooltip.component';
+import {
+    TacticsHeatmapComponent
+} from '../../global/components/tactics-pane/tactics-heatmap/tactics-heatmap.component';
+import {
+    TacticsTreemapComponent
+} from '../../global/components/tactics-pane/tactics-treemap/tactics-treemap.component';
+import {
+    TacticsCarouselComponent
+} from '../../global/components/tactics-pane/tactics-carousel/tactics-carousel.component';
+import {
+    TacticsCarouselControlComponent
+} from '../../global/components/tactics-pane/tactics-carousel/tactics-carousel-control.component';
+import {
+    TacticsTooltipComponent
+} from '../../global/components/tactics-pane/tactics-tooltip/tactics-tooltip.component';
 import { TacticsTooltipService } from '../../global/components/tactics-pane/tactics-tooltip/tactics-tooltip.service';
 import { TacticsControlService } from '../../global/components/tactics-pane/tactics-control.service';
 import { HeatmapComponent } from '../../global/components/heatmap/heatmap.component';
 import { TreemapComponent } from '../../global/components/treemap/treemap.component';
 import { CapitalizePipe } from '../../global/pipes/capitalize.pipe';
 import { AuthService } from '../../core/services/auth.service';
-import { reducers } from '../../root-store/app.reducers';
-import { RouterTestingModule } from '@angular/router/testing';
+import {
+    mockUser,
+    mockTactics,
+    mockAttackPatternData
+} from '../../global/components/tactics-pane/tactics.model.test';
+import * as configActions from '../../root-store/config/config.actions';
+import * as userActions from '../../root-store/users/user.actions';
+import { reducers, AppState } from '../../root-store/app.reducers';
+import { AttackPattern } from '../../models';
 
 describe('ThreatTacticsComponent', () => {
 
-    let component: ThreatTacticsComponent;
     let fixture: ComponentFixture<ThreatTacticsComponent>;
+    let component: ThreatTacticsComponent;
+    let store: Store<AppState>;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -69,11 +88,19 @@ describe('ThreatTacticsComponent', () => {
     beforeEach(() => {
         fixture = TestBed.createComponent(ThreatTacticsComponent);
         component = fixture.componentInstance;
+        store = TestBed.get(Store);
+        store.dispatch(new userActions.LoginUser(mockUser));
+        store.dispatch(new configActions.LoadTactics(mockTactics));
         fixture.detectChanges();
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should handle input targets', () => {
+        component.attackPatterns = [new AttackPattern()];
+        component.ngOnChanges();
     });
 
 });
