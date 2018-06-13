a9fbd5736b46b5c71004e04aba0f0a1c15ae2258
j987987 <j987897@users.noreply.github.com>
2018-06-13 16:00:49 -0400

    added bottom banner and additional config options

--- src/app/core/services/run-config.service.ts
+++ src/app/core/services/run-config.service.ts
@@ -6,11 +6,31 @@ import * as public_config from '../../../assets/public-config.json';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 
+export interface PublicConfigRoots {
+    demo: PublicConfig;
+    test: PublicConfig;
+    uac:  PublicConfig;
+}
+
+export interface PublicConfig {
+    showBanner?:   boolean;
+    bannerText?:   string;
+    authServices?: string[] | null;
+}
+
+export interface MasterConfig extends PublicConfig {
+    contentOwner?: string;
+    pagePublisher?: string;
+    lastReviewed?: number;
+    lastModified?: number;
+    footerTextHtml?: string;
+}
+
 @Injectable()
 export class RunConfigService {
 
     public readonly runMode = environment.runMode.toLocaleLowerCase();
-    public _config: Observable<any>;
+    public _config: Observable<MasterConfig>;
 
     constructor(
         private http: HttpClient,
@@ -19,15 +39,15 @@ export class RunConfigService {
     }
     
     private loadPrivateConfig() {
-        this._config = this.http.get('./assets/private-config.json').catch(() => {
+        this._config = this.http.get<MasterConfig>('./assets/private-config.json').catch(() => {
             console.warn('Could not load assets/private-config.json. Default configuration will be used.');
             console.warn('If you create or edit the file, be sure to restart the application.');
-            return Observable.of({});
+            return Observable.of({} as MasterConfig);
         });
     }
 
-    public get config(): Observable<any> {
-        return this._config.map(cfg => ({...public_config[this.runMode], ...cfg}));
+    public get config(): Observable<MasterConfig> {
+        return this._config.map(cfg => ({...public_config[this.runMode] as PublicConfig, ...cfg}));
     }
 
 }
--- src/app/global/components/footer/footer.component.ts
+++ src/app/global/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RunConfigService } from '../../../core/services/run-config.service';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  public showBanner = false;
+  public contentOwner: string = null;
+  public pagePublisher: string = null;
+  public lastReviewed: number = null;
+  public lastModified: number = null;
+  public footerTextHtml: string = null;
 
-  constructor() { }
+  constructor(private runConfigService: RunConfigService) { }
 
   ngOnInit() {
+    this.runConfigService.config.subscribe(
+      (cfg) => {
+        if (cfg) {
+          this.showBanner = cfg.showBanner || false;
+          this.contentOwner = cfg.contentOwner || null;
+          this.pagePublisher = cfg.pagePublisher || null;
+          this.lastReviewed = cfg.lastReviewed || null;
+          this.lastModified = cfg.lastModified || null;
+          this.footerTextHtml = cfg.footerTextHtml || null;
+        }
+      }
+    );
   }
 
 }
