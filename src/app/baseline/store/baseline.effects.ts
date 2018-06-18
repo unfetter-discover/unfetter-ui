4301f629165b085e9cb54f87e5ba512c6013760e
j987987 <j987897@users.noreply.github.com>
2018-06-18 13:37:55 -0400

    updated baseliens with rxjs 6

--- src/app/baseline/store/baseline.effects.ts
+++ src/app/baseline/store/baseline.effects.ts
@@ -39,45 +39,51 @@ export class BaselineEffects {
     @Effect()
     public fetchBaseline = this.actions$
         .ofType(baselineActions.FETCH_BASELINE)
-        .pluck('payload')
-        .switchMap((baselineId: string) => {
-            return this.baselineService.fetchAssessmentSet(baselineId);
-        })
-        .map((assessmentSet: AssessmentSet) => {
-            return new baselineActions.SetAndReadAssessmentSet(assessmentSet);
-        });
+        .pipe(
+            pluck('payload'),            
+            switchMap((baselineId: string) => {
+                return this.baselineService.fetchAssessmentSet(baselineId);
+            }),            
+            map((assessmentSet: AssessmentSet) => {
+                return new baselineActions.SetAndReadAssessmentSet(assessmentSet);
+            })
+        );
     
     @Effect()
     public setAndReadAssessmentSet = this.actions$
         .ofType(baselineActions.SET_AND_READ_ASSESSMENT_SET)
-        .pluck('payload')
-        .switchMap((assessmentSet: AssessmentSet) => {
-            return this.baselineService.fetchObjectAssessmentsByAssessmentSet(assessmentSet);
-        })
-        .map((objAssessments: ObjectAssessment[]) => {
-            return new baselineActions.SetAndReadObjectAssessments(objAssessments);
-        });
+        .pipe(
+            pluck('payload'),
+            switchMap((assessmentSet: AssessmentSet) => {
+                return this.baselineService.fetchObjectAssessmentsByAssessmentSet(assessmentSet);
+            }),
+            map((objAssessments: ObjectAssessment[]) => {
+                return new baselineActions.SetAndReadObjectAssessments(objAssessments);
+            })
+        );
 
     @Effect()
     public setAndReadObjectAssessments = this.actions$
         .ofType(baselineActions.SET_AND_READ_OBJECT_ASSESSMENTS)
-        .pluck('payload')
-        .switchMap((objAssessments: ObjectAssessment[]) => {
-            const observables = objAssessments
+        .pipe(
+            pluck('payload'),
+            switchMap((objAssessments: ObjectAssessment[]) => {
+                const observables = objAssessments
                 .map((objAssessment) => {
                     return this.baselineService.fetchCapability(objAssessment.object_ref);
                 });
-
-            return Observable.forkJoin(...observables).pipe(
-                map((arr) => {
-                    return new baselineActions.SetAndReadCapabilities(arr);
-                }),
-                catchError((err) => {
-                    console.log(err);
-                    return Observable.of(new baselineActions.FailedToLoad(true));
-                })
-            );
-        });
+                
+                return observableForkJoin(...observables).pipe(
+                    map((arr) => {
+                        return new baselineActions.SetAndReadCapabilities(arr);
+                    }),
+                    catchError((err) => {
+                        console.log(err);
+                        return Observable.of(new baselineActions.FailedToLoad(true));
+                    })
+                );
+            })
+        );
 
     @Effect()
     public setAndReadCapabilities = this.actions$
@@ -123,48 +129,56 @@ export class BaselineEffects {
     @Effect()
     public fetchCapabilityGroups = this.actions$
         .ofType(baselineActions.FETCH_CAPABILITY_GROUPS)
-        .switchMap(() => this.baselineService.getCategories())
-        .map((arr: Category[]) => new baselineActions.SetCapabilityGroups(arr));
+        .pipe(
+            switchMap(() => this.baselineService.getCategories()),
+            map((arr: Category[]) => new baselineActions.SetCapabilityGroups(arr))
+        );
 
     @Effect()
     public fetchCapabilities = this.actions$
         .ofType(baselineActions.FETCH_CAPABILITIES)
-        .switchMap(() => this.baselineService.getCapabilities())
-        .map((arr: Category[]) => new baselineActions.SetCapabilities(arr));
+        .pipe(
+            switchMap(() => this.baselineService.getCapabilities()),
+            map((arr: Category[]) => new baselineActions.SetCapabilities(arr))
+        );
 
     @Effect()
     public fetchAttackPatterns = this.actions$
         .ofType(baselineActions.FETCH_ATTACK_PATTERNS)
-        .pluck('payload')
-        .switchMap((selectedFramework: string) => {
-            const o1$ = Observable.of(selectedFramework);
-            // select all the attack patterns
-            const o2$ = this.attackPatternService
-                .fetchAttackPatterns()
-                .catch((ex) => Observable.of([] as AttackPattern[]));
-            // merge selected framework and all system attack patterns                
-            return Observable.forkJoin(o1$, o2$);
-        })
-        .mergeMap(([framework, allAttackPatterns]) => {
-            // if no framework given, use all attack patterns
-            let selectedAttackPatterns = allAttackPatterns;
-            if (framework) {
-                // filter if we are given a selected framework
-                const isFromSelectedFramework = (el) => {
-                    return el 
+        .pipe(
+
+            pluck('payload'),
+            switchMap((selectedFramework: string) => {
+                const o1$ = observableOf(selectedFramework);
+                // select all the attack patterns
+                const o2$ = this.attackPatternService.fetchAttackPatterns()
+                    .pipe(
+                        catchError((ex) => observableOf([] as AttackPattern[]))
+                    );
+                // merge selected framework and all system attack patterns                
+                return observableForkJoin(o1$, o2$);
+            }),
+            mergeMap(([framework, allAttackPatterns]) => {
+                // if no framework given, use all attack patterns
+                let selectedAttackPatterns = allAttackPatterns;
+                if (framework) {
+                    // filter if we are given a selected framework
+                    const isFromSelectedFramework = (el) => {
+                        return el 
                         && el.kill_chain_phases
                         .findIndex((_) => _.kill_chain_name === framework) > -1;
-                };
-                selectedAttackPatterns = allAttackPatterns
+                    };
+                    selectedAttackPatterns = allAttackPatterns
                     .filter(isFromSelectedFramework);
-            }
-
-            // tell reducer to set the attack pattern states
-            return [
-                new baselineActions.SetAttackPatterns(allAttackPatterns),
-                new baselineActions.SetSelectedFrameworkAttackPatterns(selectedAttackPatterns),
-            ];
-        });
+                }
+                
+                // tell reducer to set the attack pattern states
+                return [
+                    new baselineActions.SetAttackPatterns(allAttackPatterns),
+                    new baselineActions.SetSelectedFrameworkAttackPatterns(selectedAttackPatterns),
+                ];
+            })
+        )
     
         /**
          * Start an assessment, persist AssessmentSet to db.
@@ -172,31 +186,32 @@ export class BaselineEffects {
     @Effect()
     public startAssessment = this.actions$
         .ofType(baselineActions.START_BASELINE)
-        .pluck('payload')
-        .switchMap((el: AssessmentSet) => {
-
-            // create baselinemeta object
-            let baselineMeta = new BaselineMeta;
-            baselineMeta.title = el.name; 
-            baselineMeta.description = el.description;
-            baselineMeta.created_by_ref = el.created_by_ref;
-
-            this.baselineStateService.saveCurrent(baselineMeta);
-
-            const json = { data: { attributes: el } } as JsonApi<JsonApiData<AssessmentSet>>;
-            let url = 'api/v3/x-unfetter-assessment-sets';
-            return this.genericServiceApi.postAs<AssessmentSet>(url, json);
-
-        })
-
-        .do((el: AssessmentSet) => {
-            this.router.navigate([
-                '/baseline/wizard/new'
-            ]);
-        })
-        .map((newAssessmentSet) => {
-            return new baselineActions.SetBaseline(newAssessmentSet[0].attributes);
-        })
+        .pipe(
+            pluck('payload'),
+            switchMap((el: AssessmentSet) => {
+                
+                // create baselinemeta object
+                let baselineMeta = new BaselineMeta;
+                baselineMeta.title = el.name; 
+                baselineMeta.description = el.description;
+                baselineMeta.created_by_ref = el.created_by_ref;
+                
+                this.baselineStateService.saveCurrent(baselineMeta);
+                
+                const json = { data: { attributes: el } } as JsonApi<JsonApiData<AssessmentSet>>;
+                let url = 'api/v3/x-unfetter-assessment-sets';
+                return this.genericServiceApi.postAs<AssessmentSet>(url, json);
+                
+            }),           
+            tap((el: AssessmentSet) => {
+                this.router.navigate([
+                    '/baseline/wizard/new'
+                ]);
+            }),
+            map((newAssessmentSet) => {
+                return new baselineActions.SetBaseline(newAssessmentSet[0].attributes);
+            })
+        );
         
 
     @Effect()
