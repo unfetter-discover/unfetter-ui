bb22c2385314460885e3e5050d2895cf8ba385d8
j987987 <j987897@users.noreply.github.com>
2018-06-22 14:59:35 -0400

    began unit tests

--- src/app/global/components/selection-list/selection-list.component.spec.ts
+++ src/app/global/components/selection-list/selection-list.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SelectionListComponent } from './selection-list.component';
+import { NO_ERRORS_SCHEMA } from '@angular/compiler/src/core';
+import { MatInputModule, MatListModule, MatCheckboxModule } from '@angular/material';
+import { ChangeDetectorRef } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 describe('SelectionListComponent', () => {
   let component: SelectionListComponent;
@@ -8,7 +12,18 @@ describe('SelectionListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SelectionListComponent ]
+      declarations: [ SelectionListComponent ],
+      schemas: [NO_ERRORS_SCHEMA],
+      imports: [        
+        FormsModule,
+        ReactiveFormsModule,
+        MatInputModule,
+        MatListModule,
+        MatCheckboxModule,
+      ],
+      providers: [
+        { provide: ChangeDetectorRef, useValue: {} }
+      ]
     })
     .compileComponents();
   }));
