be06d1954ee067cca2b91d9ff92b4d4465e31c27
j987987 <30236110+j987987@users.noreply.github.com>
2018-06-26 12:31:05 -0400

    added notification service, changed notification processing to api (#527)

--- src/app/root-store/notification/notification.effects.ts
+++ src/app/root-store/notification/notification.effects.ts
@@ -1,18 +1,14 @@
 
 import { map, tap, mergeMap, switchMap } from 'rxjs/operators';
+import { of as observableOf } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { Actions, Effect } from '@ngrx/effects';
-import { Observable } from 'rxjs';
-
-
-
-
 
 import * as notificationActions from '../../root-store/notification/notification.actions';
 import { WebsocketService } from '../../core/services/web-socket.service';
 import { WSMessageTypes } from '../../global/enums/ws-message-types.enum';
-import { GenericApi } from '../../core/services/genericapi.service';
-import { AppNotification, NotificationEmitTypes } from './notification.model';
+import { AppNotification } from './notification.model';
+import { NotificationsService } from '../../core/services/notifications.service';
 
 @Injectable()
 export class NotificationEffects {
@@ -29,7 +25,7 @@ export class NotificationEffects {
     @Effect()
     public notificationStore = this.actions$
         .ofType(notificationActions.FETCH_NOTIFICATION_STORE).pipe(
-        switchMap(() => this.genericApi.get('api/notification-store/user-notifications')),
+        switchMap(() => this.notificationsService.getNotifications()),
         tap((notifications) => console.log(notifications)),
         mergeMap((notifications: any[]) => [
             new notificationActions.SetNotifications(
@@ -44,53 +40,53 @@ export class NotificationEffects {
 
     @Effect()
     public readNotification = this.actions$
-        .ofType(notificationActions.EMIT_READ_NOTIFCATION).pipe(
-        tap((action: {type: string, payload: AppNotification}) => {
-            this.websocketService.sendMessage({
-                messageType: NotificationEmitTypes.READ_NOTIFICATION,
-                messageContent: action.payload._id
-            });
-        }),
-        map((action: { type: string, payload: AppNotification }) => {
-            const readNotification = action.payload;
-            readNotification.read = true;
-            return new notificationActions.UpdateNotification(readNotification);
-        }));
+        .ofType(notificationActions.EMIT_READ_NOTIFCATION)
+        .pipe(
+            switchMap((action: {type: string, payload: AppNotification}) => {
+                return this.notificationsService.readNotification(action.payload._id)
+                    .pipe(
+                        switchMap(() => observableOf(action))
+                    );
+            }),
+            map((action) => {
+                const readNotification = action.payload;
+                readNotification.read = true;
+                return new notificationActions.UpdateNotification(readNotification);
+            })
+        );
 
     @Effect()
     public deleteNotification = this.actions$
-        .ofType(notificationActions.EMIT_DELETE_NOTIFCATION).pipe(
-        tap((action: { type: string, payload: AppNotification }) => {
-            this.websocketService.sendMessage({
-                messageType: NotificationEmitTypes.DELETE_NOTIFICATION,
-                messageContent: action.payload
-            });
-        }),
-        map((action: { type: string, payload: string }) => new notificationActions.DeleteNotification(action.payload)));
+        .ofType(notificationActions.EMIT_DELETE_NOTIFCATION)
+        .pipe(
+            switchMap((action: { type: string, payload: string }) => {
+                return this.notificationsService.deleteNotification(action.payload)
+                    .pipe(
+                        switchMap(() => observableOf(action))
+                    );
+            }),
+            map((action) => new notificationActions.DeleteNotification(action.payload))
+        );
 
     @Effect()
     public readAllNotifications = this.actions$
-        .ofType(notificationActions.EMIT_READ_ALL_NOTIFCATIONS).pipe(
-        tap((action: { type: string, payload: AppNotification }) => {
-            this.websocketService.sendMessage({
-                messageType: NotificationEmitTypes.READ_ALL_NOTIFICATIONS
-            });
-        }),
-        map(() => new notificationActions.MarkAllAsRead()));
+        .ofType(notificationActions.EMIT_READ_ALL_NOTIFCATIONS)
+        .pipe(
+            switchMap(() => this.notificationsService.readAllNotifications()),
+            map(() => new notificationActions.MarkAllAsRead())
+        );
 
     @Effect()
     public deleteAllNotifications = this.actions$
-        .ofType(notificationActions.EMIT_DELETE_ALL_NOTIFCATIONS).pipe(
-        tap((action: { type: string, payload: AppNotification }) => {
-            this.websocketService.sendMessage({
-                messageType: NotificationEmitTypes.DELETE_ALL_NOTIFICATIONS
-            });
-        }),
-        map(() => new notificationActions.DeleteAllNotifications()));
+        .ofType(notificationActions.EMIT_DELETE_ALL_NOTIFCATIONS)
+        .pipe(
+            switchMap(() => this.notificationsService.deleteAllNotifications()),
+            map(() => new notificationActions.DeleteAllNotifications())
+        );
 
     constructor(
         private actions$: Actions,
         private websocketService: WebsocketService,
-        private genericApi: GenericApi
+        private notificationsService: NotificationsService
     ) { }
 }
--- src/app/utils/constance.ts
+++ src/app/utils/constance.ts
@@ -110,6 +110,7 @@ export const Constance = {
   PROFILE_BY_ID_URL: 'api/auth/profile',
   REFRESH_TOKEN_URL: 'api/auth/refreshtoken',
   PUBLIC_CONFIG_URL: 'api/auth/public-config',
+  NOTIFICATION_STORE_URL: 'api/notification-store',
 
   CONFIG_URL: 'api/config',
   WEB_ANALYTICS_URL: 'api/web-analytics',
