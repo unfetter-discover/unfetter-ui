0eb7ba6977a7986350d4af745835f78f3e109222
j987987 <j987897@users.noreply.github.com>
2018-06-27 14:46:19 -0400

    streamlined avatar urls

--- src/app/root-store/users/users.reducers.ts
+++ src/app/root-store/users/users.reducers.ts
@@ -1,5 +1,6 @@
 import * as userActions from './user.actions';
 import { UserProfile } from '../../models/user/user-profile';
+import { UserHelpers } from '../../global/static/user-helpers';
 
 export interface UserState {
     userProfile: UserProfile,
@@ -20,13 +21,15 @@ export const initialState: UserState = {
 export function usersReducer(state = initialState, action: userActions.UserActions) {
     switch (action.type) {
         case userActions.LOGIN_USER:
+            const avatar_url = UserHelpers.getAvatarUrl(action.payload.userData);
             return {
                 ...state,
                 userProfile: action.payload.userData,
                 token: action.payload.token,
                 role: action.payload.userData.role,
                 approved: action.payload.userData.approved,
-                authenticated: true
+                authenticated: true,
+                avatar_url
             };
         case userActions.UPDATE_USER_DATA:
             return {
