c5e3fee7e2c374c033f902fef31d4dd8094d3abe
sbertrand <sbertrand@mitre.org>
2018-06-28 21:31:14 -0400

    Exposed published value for baselines master list display

--- src/app/baseline/models/last-modified-baseline.ts
+++ src/app/baseline/models/last-modified-baseline.ts
@@ -4,5 +4,5 @@ import { LastModifiedStix } from '../global/models/last-modified-stix';
  * @description object returned from the last modified baselines 3.0 aggregate query
  */
 export class LastModifiedBaseline extends LastModifiedStix {
-    assessments: string[];
+    published: string;
 }
