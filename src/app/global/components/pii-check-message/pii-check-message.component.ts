6fad958cb7a33ae9d06807f057684f5f950799c8
j987987 <j987897@users.noreply.github.com>
2018-08-15 10:00:06 -0400

    made PiiCheckMessageComponent vastly more efficient

--- src/app/global/components/pii-check-message/pii-check-message.component.ts
+++ src/app/global/components/pii-check-message/pii-check-message.component.ts
@@ -1,20 +1,34 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
 import { CheckPII } from '../../static/check-pii';
+import { debounceTime, finalize } from '../../../../../node_modules/rxjs/operators';
 
 @Component({
   selector: 'pii-check-message',
   templateUrl: './pii-check-message.component.html',
   styleUrls: ['./pii-check-message.component.scss']
 })
-export class PiiCheckMessageComponent {
+export class PiiCheckMessageComponent implements OnInit {
 
   @Input() public formCtrl: FormControl;
+  public piiWarnings: any[] = [];
   
   constructor() { }
 
-  public getPiiWarning(inputString: string): any[] {
-    return CheckPII.validationErrors(inputString);
+  public ngOnInit() {
+    const formChange$ = this.formCtrl.valueChanges
+      .pipe(
+        debounceTime(100),
+        finalize(() => formChange$ && formChange$.unsubscribe())
+      )
+      .subscribe(
+        (formVal) => {
+          this.piiWarnings = CheckPII.validationErrors(formVal);
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
   }
 }
