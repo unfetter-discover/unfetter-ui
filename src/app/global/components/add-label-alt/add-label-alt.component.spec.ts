4220844b6e322372004c748ad3c216a52b741322
j987987 <j987897@users.noreply.github.com>
2018-08-27 12:46:58 -0400

    added auto complete to add labels alt component

--- src/app/global/components/add-label-alt/add-label-alt.component.spec.ts
+++ src/app/global/components/add-label-alt/add-label-alt.component.spec.ts
@@ -1,9 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { MatButtonModule, MatInputModule } from '@angular/material';
+import { MatButtonModule, MatInputModule, MatAutocompleteModule } from '@angular/material';
 import { ReactiveFormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
 
+import * as fromRoot from '../../../root-store/app.reducers';
 import { AddLabelAltComponent } from './add-label-alt.component';
 import { CapitalizePipe } from '../../pipes/capitalize.pipe';
 import { IndicatorForm } from '../../form-models/indicator';
@@ -22,7 +24,9 @@ describe('AddLabelAltComponent', () => {
         NoopAnimationsModule,
         ReactiveFormsModule,
         MatButtonModule,
-        MatInputModule
+        MatInputModule,
+        MatAutocompleteModule,
+        StoreModule.forRoot(fromRoot.reducers)
       ],
       schemas: [NO_ERRORS_SCHEMA]
     })
--- src/app/global/components/add-label-alt/add-label-alt.component.ts
+++ src/app/global/components/add-label-alt/add-label-alt.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit, Input, ChangeDetectorRef } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, withLatestFrom, startWith, finalize } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
+import { StixCoreEnum } from 'stix';
+
+import { AppState } from '../../../root-store/app.reducers';
+import { ConfigState } from '../../../root-store/config/config.reducers';
 
 @Component({
   selector: 'add-label-alt',
@@ -8,16 +15,74 @@ import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 })
 export class AddLabelAltComponent implements OnInit {
 
+  /** @required */
   @Input() public parentForm: FormGroup;
+  /** @required */
+  @Input() public stixType: StixCoreEnum | string;
   @Input() public parentDocumentType: string = 'analytic';
 
   public newLabel: FormControl;
-  public formResetComplete = true;  
+  public openVocabList: string[] = [];
+  public formResetComplete = true;
 
-  constructor(private changeDetectorRef: ChangeDetectorRef) { }
+  constructor(
+    private changeDetectorRef: ChangeDetectorRef,
+    private store: Store<AppState>
+  ) { }
 
   public ngOnInit() {
     this.resetForm();
+
+    const getVocab$ = this.parentForm.get('labels').valueChanges
+      .pipe(
+        startWith(this.parentForm.get('labels').value),
+        withLatestFrom(
+          this.store.select('config')
+        ),
+        map(([currentLabels, configState]: [string[], ConfigState]) => {
+          if (configState.configurations.openVocab) {
+            let localVocabList;
+            switch (this.stixType) {
+              case 'indicator':
+                localVocabList = configState.configurations.openVocab['indicator-label-ov'].enum;
+                break;
+              case 'identity':
+                localVocabList = configState.configurations.openVocab['identity-label-ov'].enum;
+                break;
+              case 'malware':
+                localVocabList = configState.configurations.openVocab['malware-label-ov'].enum;
+                break;
+              case 'report':
+                localVocabList = configState.configurations.openVocab['report-label-ov'].enum;
+                break;
+              case 'threat-actor':
+                localVocabList = configState.configurations.openVocab['threat-actor-label-ov'].enum;
+                break;
+              case 'tool':
+                localVocabList = configState.configurations.openVocab['tool-label-ov'].enum;
+                break;
+              default:
+                localVocabList = [];
+            }
+
+            if (currentLabels && currentLabels.length) {
+              return localVocabList.filter((ov) => !currentLabels.includes(ov)).sort();
+            } else {
+              return localVocabList;
+            }
+          } else {
+            return [];
+          }
+        }),
+        finalize(() => getVocab$ && getVocab$.unsubscribe())
+      )
+      .subscribe((ovList) => {
+          this.openVocabList = ovList;
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
   }
 
   public resetForm() {
