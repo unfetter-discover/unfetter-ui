facd0d0757abe1e95bfa90ff6fa8dfc1b47c43bc
webiator <6414732+webiator@users.noreply.github.com>
2018-10-19 13:22:46 -0400

    Reset fdescribe

--- src/app/threat-beta/feed/activity-list/activity-list.component.spec.ts
+++ src/app/threat-beta/feed/activity-list/activity-list.component.spec.ts
@@ -26,7 +26,7 @@ import * as boardActions from '../../store/threat.actions';
 import { threatReducer } from '../../store/threat.reducers';
 import { StixCoreEnum } from 'stix';
 
-fdescribe('ActivityListComponent', () => {
+describe('ActivityListComponent', () => {
 
     let fixture: ComponentFixture<ActivityListComponent>;
     let component: ActivityListComponent;
