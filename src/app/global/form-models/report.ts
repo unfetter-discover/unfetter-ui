5881ef8901ef47937133aeaa002d5512ce2e0180
j987987 <j987897@users.noreply.github.com>
2018-11-13 16:19:02 -0500

    uploaded by url

--- src/app/global/form-models/report.ts
+++ src/app/global/form-models/report.ts
@@ -1,13 +1,19 @@
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { ExtractTextSupportedFileTypes } from '../enums/extract-text-file-types.enum';
+import { ExternalReferencesForm } from './external-references';
 
 export const ReportForm = () => {
     return new FormGroup({
         created_by_ref: new FormControl('', Validators.required),
         name: new FormControl('', Validators.required),
         description: new FormControl(),
-        external_references: new FormArray([]),
+        external_references: new FormArray([ExternalReferencesForm()]),
         labels: new FormArray([]),
         metaProperties: new FormGroup({
+            extractedText: new FormGroup({
+                fileType: new FormControl(ExtractTextSupportedFileTypes.PDF),
+                method: new FormControl('URL') 
+            }),
             published: new FormControl(true)
         }),
         object_marking_refs: new FormControl([]),
--- src/app/threat-beta/report-form/report-form.component.ts
+++ src/app/threat-beta/report-form/report-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { Location } from '@angular/common';
 import { Store } from '@ngrx/store';
@@ -8,21 +8,27 @@ import { pluck, map } from 'rxjs/operators';
 import { ReportForm } from '../../global/form-models/report';
 import { AppState } from '../../root-store/app.reducers';
 import MarkingDefinitionHelpers from '../../global/static/marking-definition-helper';
+import { ExtractTextSupportedFileTypes } from '../../global/enums/extract-text-file-types.enum';
+import { ExtractTextService } from '../../core/services/extract-text.service';
+import { OpenSnackbar } from '../../root-store/utility/utility.actions';
 
 @Component({
   selector: 'report-form',
   templateUrl: './report-form.component.html',
-  styleUrls: ['./report-form.component.scss']
+  styleUrls: ['./report-form.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ReportFormComponent implements OnInit {
 
   public form: FormGroup = ReportForm();
   public editMode = false;
   public marking$: Observable<any>;
+  public supportedFileTypes: string[] = Object.values(ExtractTextSupportedFileTypes).concat('Other');
 
   constructor(
     public location: Location,
-    private store: Store<AppState>
+    private store: Store<AppState>,
+    private extractTextService: ExtractTextService
   ) { }
 
   ngOnInit() {
@@ -38,6 +44,24 @@ export class ReportFormComponent implements OnInit {
             };
           });
         })
+      );
+  }
+
+  public extractTextByUrl() {
+    if (this.form.get('external_references').get('0').status === 'INVALID') {
+      console.warn('Attempt to extract text by url without a valid form');
+      return;
+    }
+    const url = this.form.get('external_references').get('0').get('url').value;
+    const fileType = this.form.get('metaProperties').get('extractedText').get('fileType').value;
+    this.extractTextService.extractTextFromUrl(url, fileType)
+      .subscribe(
+        ({ extractedText }) => {
+          this.form.get('description').setValue(extractedText)
+        },
+        (err) => {
+          this.store.dispatch(new OpenSnackbar('Unable to extract text'));
+        }
       );
   }
 
